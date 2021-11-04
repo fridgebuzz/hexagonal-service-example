34220f26b83e45e7779f7b721f7e45f5c3f411fb
Vanessa Williams <vanessa@fridgebuzz.com>
2021-11-04 14:31:16 -0400

    Removing pairing code from main

--- src/main/java/com/spothero/parking/ParkingRateApplication.java
+++ src/main/java/com/spothero/parking/ParkingRateApplication.java
@@ -3,13 +3,10 @@ package com.spothero.parking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class ParkingRateApplication
 {
-	public static void main(String[] args) throws IOException
-    {
-        SpringApplication.run(ParkingRateApplication.class, args);
-	}
+	public static void main(String[] args) {
+      SpringApplication.run(ParkingRateApplication.class, args);
+  }
 }
